5249b6b3420b3c7559616c71d121ddc77adf24b2
Caden Huffman <cadenhuf@bu.edu>
2024-01-05 23:00:25 -0500

    Commenting Model Tester

--- tester.py
+++ tester.py
@@ -5,6 +5,7 @@ class Model:
 
 
     def __init__(self):
+        #Init for tester
         self.q_table = np.zeros((9,4))
         self.state_to_row = {(0,1):1,}
         self.inaccessible_cols = {0: [0, 3], 1: [3], 2: [1,3], 3: [0], 5: [1], 6: [0, 2], 7: [2], 8: [1, 2]}
@@ -13,7 +14,7 @@ class Model:
         self.R = 1
         
 
-
+        
     def predict(self,state):
         #State is array representing position of Agent, [0,1] = spot (0,1) on board
         row_num = self.state_to_row[state]
